df33378ec0a6d9154bcc7bc5f79e929523f8def2
ta-moli <yarnusha7@gmail.com>
2021-08-24 00:59:54 +0300

    change4

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-const user= {
-   name: 'Mango',
-   age: 2,
-};
+// const user= {
+//    name: 'Mango',
+//    age: 2,
+// };
  
-console.log(JSON.stringify(user));
+// console.log(JSON.stringify(user));
 
-const saveUserData = '{'name':'Mango','age':2}';
+// const saveUserData = '{"name":"Mango","age":2}';
 
-console.log(JSON.parse(saveUserData));
+// console.log(JSON.parse(saveUserData));
+
+console.log(localStorage);
+
+localStorage.setItem('my-data', JSON.stringify({ name: "Mango", age: 2 }));
+
+const savedData = localStorage.getItem('my-data');
+console.log('savedData', savedData);
+
+const parsedData = JSON.parse(savedData);
+console.log('parsedData', parsedData);
