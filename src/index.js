9fcb8b218897ea50a440a95b6d0cf52276b3d037
ta-moli <yarnusha7@gmail.com>
2021-08-25 23:12:51 +0300

    change

--- src/index.js
+++ src/index.js
@@ -9,12 +9,41 @@
 
 // console.log(JSON.parse(saveUserData));
 
-console.log(localStorage);
+// console.log(localStorage);
 
-localStorage.setItem('my-data', JSON.stringify({ name: "Mango", age: 2 }));
+// localStorage.setItem('my-data', JSON.stringify({ name: "Mango", age: 2 }));
 
-const savedData = localStorage.getItem('my-data');
-console.log('savedData', savedData);
+// const savedData = localStorage.getItem('my-data');
+// console.log('savedData', savedData);
 
-const parsedData = JSON.parse(savedData);
-console.log('parsedData', parsedData);
+// const parsedData = JSON.parse(savedData);
+// console.log('parsedData', parsedData);
+
+import './css/common.css';
+import './css/feedback-form.css';
+
+const refs = {
+  form: document.querySelector('.js-feedback-form'),
+  textarea: document.querySelector('.js-feedback-form  textarea'),
+};
+
+// populateMessage();
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+
+function onTextareaInput(evt) {
+   const value = evt.currentTarget.value;
+
+   console.log(value);
+}
+
+function onFormSubmit(evt) {
+   evt.preventDefault();
+ 
+   console.log('Отправляем форму');
+   // localStorage.removeItem('feedback-message');
+   evt.currentTarget.reset();
+ }
+ 
