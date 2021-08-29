4a9a9ef7d952dfe5b3744701122f5d37b66a32f7
ta-moli <yarnusha7@gmail.com>
2021-08-30 02:14:34 +0300

    add

--- src/js/card.js
+++ src/js/card.js
@@ -3,13 +3,12 @@ import menuData from '../menu.json';
 import itemsTemplate from '../templates/template.hbs';
 
 const galleryRef = document.querySelector('.js-menu');
-
 const markup = itemsTemplate(menuData);
 galleryRef.insertAdjacentHTML('beforebegin', markup);
 
 
-// function itemsTemplate(menuData) {
-//    return menuData.map(cardsTpl).join('');
+// function createMenu(menuData) {
+//    return menuData.map(itemsTemplate).join('');
 // }
 
 // const body = document.querySelector('body');
---
+++ src/js/theme.js
@@ -0,0 +1,17 @@
+const Theme = {
+   LIGHT: 'light-theme',
+   DARK: 'dark-theme',
+ };
+
+ export default theme;
+
+//  toolbox.addEventListener("change", (e) => {
+//    body.classList.toggle("dark-theme")
+//    console.log(localStorage.getItem("class"))
+//    if (localStorage.getItem("class")) {
+//        localStorage.removeItem("class")
+//    } else {
+//        localStorage.setItem("class", "dark-theme")
+//        toolbox.checked = true;
+//    }
+// });
